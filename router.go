ab2fd8d7a8f59767a8921b4aa0d6b6b9acb940bb
shuoGG1239 <328893769@qq.com>
2019-06-16 23:04:21 +0800

    update web

--- router.go
+++ router.go
@@ -1,5 +1,7 @@
 package redtea
 
+import "github.com/gorilla/mux"
+
 type Route interface {
 	Get(urlPath string, handler interface{})
 	Post(urlPath string, handler interface{})
@@ -14,12 +16,17 @@ type Middleware interface {
 
 type router struct {
 	middlewares []Middleware
+	rt mux.Router
 }
 
 func NewRouter() *router {
 	r := new(router)
 	r.middlewares = make([]Middleware, 0, 4)
 	return r
+}
+
+func (r *router) handle(method, urlPath string, handler interface{}) {
+
 }
 
 func (r *router) Get(urlPath string, handler interface{}) {
--- web.go
+++ web.go
@@ -1,1 +1,30 @@
 package redtea
+
+type Web struct {
+	router *router
+}
+
+func NewWeb() *Web {
+	w := new(Web)
+	return w
+}
+
+func (w *Web) ServeHttp() {
+
+}
+
+func (w *Web) Get(urlPath string, handler interface{}) {
+	w.router.Get(urlPath, handler)
+}
+
+func (w *Web) Post(urlPath string, handler interface{}) {
+	w.router.Post(urlPath, handler)
+}
+
+func (w *Web) Put(urlPath string, handler interface{}) {
+	w.router.Put(urlPath, handler)
+}
+
+func (w *Web) Delete(urlPath string, handler interface{}) {
+	w.router.Delete(urlPath, handler)
+}
